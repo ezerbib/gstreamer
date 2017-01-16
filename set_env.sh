4eee36f1dc29fc33550a58448915ef1c7ce8ee81
Eric Zerbib <ezerbib@kramerel.com>
2017-01-16 17:58:23 +0200

    - remove kramer specific Signed-off-by: Eric Zerbib <ezerbib@kramerel.com>

--- set_env.sh
+++ set_env.sh
@@ -1,4 +1,4 @@
- PFX=/opt/kramer/gstreamer/
+ PFX=/opt/gstreamer/
  export PATH=$PATH:$PFX/bin
  export LD_RUN_PATH=$LD_RUN_PATH:$PFX/lib
  export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PFX/lib/pkgconfig
